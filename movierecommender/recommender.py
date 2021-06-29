3ae5fde10244be92d837bb967119ac1387eb75a4
yingha <yinghalin@gmail.com>
2021-06-29 11:00:37 +0200

    initial commit

---
+++ movierecommender/recommender.py
@@ -0,0 +1,87 @@
+"""
+Contains various recommondation implementations
+all algorithms return a list of movieids
+"""
+import numpy as np
+import pandas as pd
+from utils import movies,ratings,item_avg,final_rec,get_movie_id,get_user_item_matrix # you have already the data access of movies
+import pickle
+
+
+def recommend_random(liked_items, k=5):
+    """
+    return k random unseen movies for user 
+    """
+    # dummy implementation
+    return movies.sample(k)
+
+def recommend_most_popular(liked_items, item_avg, k=5):
+    """
+    return k most popular unseen movies for user
+    """
+    item_names, liked_item_ids = get_movie_id(liked_items)
+    recommendations= item_avg.sort_values(ascending=False)
+    item_filter = ~recommendations.index.isin(liked_item_ids) 
+    recommendations = final_rec(recommendations,liked_item_ids,k)
+    
+    return recommendations
+
+def personalized_recommender(liked_items, item_rating, k=5):
+    '''
+    gets a user list of watched movies, the item averages 
+    and returns a list of k movie_ids based on user's preferance
+    Neighborhood-based Collaborative Filtering (NearestNeighbors)
+    '''
+    item_names, liked_item_ids = get_movie_id(liked_items)
+    with open('./models/movie_recommender_model1.pickle', 'rb') as file:
+        model = pickle.load(file)
+    user_vec = np.repeat(0, 193610)
+    user_vec[liked_item_ids]=item_rating
+    distances, user_ids = model.kneighbors([user_vec], n_neighbors=5)
+    neighborhood = ratings.set_index('userId').loc[user_ids[0]]
+    recommendations = neighborhood.groupby('movieId')['rating'].sum().sort_values(ascending=False)
+    recommendations = final_rec(recommendations,liked_item_ids,k)
+    return recommendations
+
+def personalized_recommender_nmf(liked_items, item_rating, k=5):
+    '''
+    gets a user list of watched movies, the item averages 
+    and returns a list of k movie_ids based on user's preferance
+    Collaborative Filtering with Matrix Factorization
+    '''
+    with open('./models/movie_recommender_nmf.pickle', 'rb') as file:
+        nmf = pickle.load(file)
+    user_item_matrix = get_user_item_matrix()
+    Q = nmf.components_ 
+    movie_list = list(user_item_matrix.columns)
+    item_names, liked_item_ids = get_movie_id(liked_items)
+    user_movie_rating = dict(zip(liked_item_ids, item_rating))
+    empty_list = [2.5]*len(movie_list)
+    movie_dict = dict(zip(movie_list, empty_list))
+    for movie, rating in user_movie_rating.items():
+        movie_dict[movie]=rating
+    movie_df = pd.DataFrame(list(movie_dict.values()), index=movie_dict.keys())
+    movie_df = movie_df.T
+    P = nmf.transform(movie_df)
+    predictions = np.dot(P,Q)
+    user_item_prediction = pd.DataFrame(predictions, columns=user_item_matrix.columns)
+    recommendations = user_item_prediction[(movie_df == 2.5)].round(1).T
+    recommendations.columns = ['predicted_rating']
+    recommendations=recommendations['predicted_rating'].sort_values(ascending=False)
+    print(recommendations.value_counts())
+    recommendations = final_rec(recommendations,liked_item_ids,k)
+    return recommendations
+
+
+if __name__ == '__main__':
+    #print(recommend_random([1, 2, 3]))
+    liked_items = ['star trek', 'star wars', 'toy story', 'inside out', 'zootopia', 'coco (2017)']
+    item_rating = ['4', '4', '2', '4', '4', '4']
+    #print(personalized_recommender(liked_items, item_rating, k=5))
+    #print(personalized_recommender_nmf(liked_items, item_rating, k=5))
+    print(recommend_most_popular(liked_items, item_avg, k=5))
+
+
+
+
+
---
+++ movierecommender/utils.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import pickle
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
+from sklearn.decomposition import NMF
+from fuzzywuzzy import process
+import os
+
+package_dir = os.path.dirname(__file__)
+print(package_dir)
+
+# put the movieId into the row index!
+movies = pd.read_csv(package_dir + '/data/ml-latest-small/movies.csv', index_col=0)  
+ratings = pd.read_csv(package_dir + '/data/ml-latest-small/ratings.csv')
+item_avg = ratings.groupby(['movieId'])['rating'].sum()
+                  
+
+def lookup_movie(search_query, titles):
+    """
+    given a search query, uses fuzzy string matching to search for similar 
+    strings in a pandas series of movie titles
+
+    returns a list of search results. Each result is a tuple that contains 
+    the title, the matching score and the movieId.
+    """
+    matches = process.extractBests(search_query, titles, score_cutoff=90)
+    return matches
+
+def get_movie_id(liked_items):
+    """
+    given a list of liked_items
+    return the item_names and liked_item_ids
+    one item_name only returns one liked_item_id
+    """
+    item_names = []
+    liked_item_ids=[]
+    for item in liked_items:
+        item_name = lookup_movie(item, movies['title'])
+        item_name = item_name[0][0]
+        item_names.append(item_name)
+        movie_filter = movies['title']==item_name
+        item_id = movies[movie_filter].index[0]
+        liked_item_ids.append(item_id)
+    return item_names, liked_item_ids
+
+def final_rec(recommendations,liked_item_ids,k):
+    '''
+    take predicted user_item_matrix
+    return top k unseen movies for user
+    '''
+    item_filter = ~recommendations.index.isin(liked_item_ids) 
+    recommendations = recommendations.loc[item_filter]
+    recommendations = movies.loc[recommendations.head(k).index]
+    return recommendations
+
+def get_user_item_matrix():
+    user_item_matrix = ratings.pivot(index='userId', columns='movieId', values='rating')
+    user_item_matrix.fillna(2.5, inplace=True)
+    return user_item_matrix
+
+def train_nn_model(metric):
+    '''
+    train model for movie recommender
+    Neighborhood-based Collaborative Filtering (NearestNeighbors)
+    '''
+    user_item = csr_matrix((ratings['rating'], (ratings['userId'], ratings['movieId'])))
+    model = NearestNeighbors(metric=metric)
+    model.fit(user_item)
+    return model
+
+def train_nmf_model(n_components,init, max_iter):
+    '''
+    train model for movie recommender
+    Collaborative Filtering with Matrix Factorization
+    the 
+    '''
+    user_item_matrix = get_user_item_matrix()
+    nmf = NMF(n_components=n_components, init=init,max_iter=max_iter, tol=0.001, verbose=True)
+    nmf.fit(user_item_matrix)
+    print('nmf.reconstruction_err_',nmf.reconstruction_err_)
+    return nmf
+
+if __name__ == '__main__':
+    '''
+    results = process.extractBests('baby yoda', movies['title'])
+    # [(title, score, movieId), ...]
+    print(results)    
+
+    liked_items = ['star trek', 'star wars', 'toy story','shawshank redemption']
+    item_names, liked_item_ids = get_movie_id(liked_items)
+    print(item_names)
+    print(liked_item_ids)
+
+    model = train_model('cosine')
+    with open('./models/movie_recommender_model1.pickle', 'wb') as file:
+        pickle.dump(model, file)
+
+    user_item_matrix = get_user_item_matrix()
+    print(user_item_matrix)
+    
+
+    model = train_nmf_model(n_components=55, init='nndsvd',max_iter=10000)
+    with open('./models/movie_recommender_nmf.pickle', 'wb') as file:
+        pickle.dump(model, file)
+    
+    '''
+
+
