87dccfc90b861ff88fbe14417bbee00d1f80d354
yingha <yingha@users.noreply.github.com>
2021-07-05 10:28:30 +0000

    autopep8 action fixes

--- test/test_recommender.py
+++ test/test_recommender.py
@@ -1,5 +1,6 @@
 from movierecommender.recommender import recommend_random
 
+
 def test_recommend_random():
     liked_items = ['star trek', 'star wars']
     assert len(recommend_random(liked_items)) == 5
